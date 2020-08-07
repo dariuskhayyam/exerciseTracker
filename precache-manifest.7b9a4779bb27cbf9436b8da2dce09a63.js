cd384165c21cab47cc083dc09efc78e10f16c7a5
darius khayyam <dv.Khayyam@gmail.com>
2020-08-07 11:29:28 +0200

    Updates

---
+++ precache-manifest.7b9a4779bb27cbf9436b8da2dce09a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3381c2f8bfe98b1a7fe952e187c49b",
+    "url": "/exerciseTracker/index.html"
+  },
+  {
+    "revision": "4ff95e8ba7c798099f50",
+    "url": "/exerciseTracker/static/css/main.9c3105c8.chunk.css"
+  },
+  {
+    "revision": "6d7c99fdc9ff07251d58",
+    "url": "/exerciseTracker/static/js/2.b00e3dc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exerciseTracker/static/js/2.b00e3dc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff95e8ba7c798099f50",
+    "url": "/exerciseTracker/static/js/main.b9fa2695.chunk.js"
+  },
+  {
+    "revision": "5dfceb2e402c446ee9d4",
+    "url": "/exerciseTracker/static/js/runtime-main.363f9909.js"
+  }
+]);
